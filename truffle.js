fb5582e00681d24d632c5a811345a8d070ff4dca
DioPires <diogo.m.pires@tecnico.ulisboa.pt>
2018-09-09 11:56:02 +0100

    Changed default configuration of truffle to use different networks and Infura for deployment

--- truffle.js
+++ truffle.js
@@ -1,18 +1,28 @@
-/*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
- * function when declaring them. Failure to do so will cause commands to hang. ex:
- * ```
- * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
- *     },
- *     network_id: '1',
- *     gas: 4500000,
- *     gasPrice: 10000000000,
- *   },
- */
+require('dotenv').config();
+var HDWalletProvider = require("truffle-hdwallet-provider");
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+    networks: {
+        development: {
+            host: "localhost",
+            port: 8545,
+            network_id: "*" // Match any network id
+        },
+        ropsten: {
+            provider: function() {
+              return new HDWalletProvider(process.env.INFURA_ROPSTEN_MNEMONIC, `https://ropsten.infura.io/${process.env.INFURA_KEY}`)
+            },
+            network_id: "3",
+            gas: 4000000, // Gas limit used for deploys
+            gasPrice: 20000000000
+        },
+        main: {
+            provider: function() {
+              return new HDWalletProvider(process.env.INFURA_MAINNET_MNEMONIC, `https://mainnet.infura.io/${process.env.INFURA_KEY}`)
+            },
+            network_id: "1",
+            gasPrice: 20000000000, // Be careful, this is in Shannon
+            gas: 4000000
+        }
+    }
 };
